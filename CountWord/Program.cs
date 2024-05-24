df96f01cbb65855c788e5690744466eb064758ad
Riza Anisul Fu`ad <rizaanisulf@gmail.com>
2024-05-24 14:41:47 +0700

    initial commit & Counter Letter Utility console app v1

---
+++ CountWord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+Console.WriteLine("Masukkan kata atau kalimat:");
+string userInput = Console.ReadLine();
+string output = HitungHuruf(userInput);
+
+Console.WriteLine(output);
+
+static string HitungHuruf(string text)
+{
+    text = text.ToLower();
+    Dictionary<char, int> jumlahHuruf = [];
+    foreach (char karakter in text)
+    {
+        if (char.IsLetter(karakter))
+        {
+            if (jumlahHuruf.TryGetValue(karakter, out int value))
+            {
+                jumlahHuruf[karakter] = ++value;
+            }
+            else
+            {
+                jumlahHuruf[karakter] = 1;
+            }
+        }
+    }
+
+    string hasil = string.Join(", ", jumlahHuruf.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+
+    return hasil;
+}
---
+++ CountWordOnText/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class ProgramPengelompokanHuruf {
+    static void Main() {
+
+        string[] daftarKata = ["Pendanaan", "Terproteksi", "Untuk", "Dampak", "Berarti"];
+
+        Console.WriteLine("Masukkan huruf :");
+        string hurufInput = Console.ReadLine();
+
+        Dictionary<char, int> jumlahHuruf = [];
+
+        foreach (string kata in daftarKata) {
+            foreach (char huruf in kata) {
+                if (hurufInput.IndexOf(huruf, StringComparison.OrdinalIgnoreCase) >= 0) {
+                    if (jumlahHuruf.TryGetValue(huruf, out int nilai)) {
+                        jumlahHuruf[huruf] = ++nilai;
+                    } else {
+                        jumlahHuruf[huruf] = 1;
+                    }
+                }
+            }
+        }
+
+        var hurufTerurut = jumlahHuruf.OrderByDescending(kvp => kvp.Value)
+                                      .ThenBy(kvp => kvp.Key);
+
+        List<char> hasilAkhir = [];
+        int frekuensiSebelumnya = -1;
+
+        foreach (var item in hurufTerurut) {
+            if (item.Value != frekuensiSebelumnya) {
+                var hurufBesar = hurufTerurut.Where(x => char.IsUpper(x.Key) && x.Value == item.Value)
+                                             .OrderBy(x => x.Key)
+                                             .Select(x => x.Key);
+                hasilAkhir.AddRange(hurufBesar);
+
+                var hurufKecil = hurufTerurut.Where(x => char.IsLower(x.Key) && x.Value == item.Value)
+                                             .OrderBy(x => x.Key)
+                                             .Select(x => x.Key);
+                hasilAkhir.AddRange(hurufKecil);
+
+                frekuensiSebelumnya = item.Value;
+            }
+        }
+        Console.WriteLine(hasilAkhir);
+    }
+}
